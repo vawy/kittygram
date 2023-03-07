67ea116e5a16248f666d49f355e52272e53d59b7
Vasiliy Vigilyanskiy <vasilyvig@gmail.com>
2023-03-07 19:05:33 +0300

    add frontend and change backend settings to connect backend with frontend

--- kittygram_backend/kittygram_backend/settings.py
+++ kittygram_backend/kittygram_backend/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'drf_yasg',
     'rest_framework.authtoken',
     'rest_framework',
+    'corsheaders',
     'djoser',
     'cats.apps.CatsConfig',
 ]
@@ -26,6 +27,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -111,6 +113,11 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 
 }
+
+CORS_URLS_REGEX = r'^/api/.*$'
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
 
 SWAGGER_SETTINGS = {
     'SECURITY_DEFINITIONS': {
