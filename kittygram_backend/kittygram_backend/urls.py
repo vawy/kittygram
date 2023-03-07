f67440b4791ee9d9595ce2ec258745fc44c40603
Vasiliy Vigilyanskiy <vasilyvig@gmail.com>
2023-03-07 18:57:38 +0300

    add backend

---
+++ kittygram_backend/kittygram_backend/urls.py
@@ -0,0 +1,49 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+
+from rest_framework import routers, permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+from cats.views import AchievementViewSet, CatViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'cats', CatViewSet)
+router.register(r'achievements', AchievementViewSet)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Cats API",
+        default_version='v1',
+        description="Документация для приложения cats проекта Kittygram",
+        terms_of_service="URL страницы с пользовательским соглашением",
+        contact=openapi.Contact(email="admin@kittygram.ru"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/', include('djoser.urls')),
+    path('api/', include('djoser.urls.authtoken'))
+]
+
+urlpatterns += [
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
